858673dc04e3422e97db562f70e83b648a0dd8ca
mrange <marten_range@hotmail.com>
2018-11-10 15:23:24 +0100

    Formlets

--- minimal/src/App.fs
+++ minimal/src/App.fs
@@ -1,16 +1,11 @@
 module App
 
-(**
- The famous Increment/Decrement ported from Elm.
- You can find more info about Emish architecture and samples at https://elmish.github.io/
-*)
-
 open Elmish
 open Elmish.React
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
-module Formlets2 =
+module Formlets =
   open Fable.Import.React
 
   [<RequireQualifiedAccess>]
@@ -89,75 +84,84 @@ module Formlets2 =
     | D of (ModelUpdate -> unit)
 
     static member Value (D d) v     : unit        = d (ModelUpdate.Value v)
-    static member Left (D d)        : Dispatcher  = D (fun mu -> d (ModelUpdate.Left mu))
+    static member Left  (D d)       : Dispatcher  = D (fun mu -> d (ModelUpdate.Left mu))
     static member Right (D d)       : Dispatcher  = D (fun mu -> d (ModelUpdate.Right mu))
 
-  type Formlet<'T> = F of (FormletPath -> Model -> Dispatcher -> 'T*ViewTree*FailureTree)
+  type Formlet<'T> = F of (FormletPath -> IHTMLProp list -> Model -> Dispatcher -> 'T*ViewTree*FailureTree)
 
   module Formlet =
     module Details =
-      let inline adapt  (F f)     = f
-      let inline invoke f fp m d  = f fp m d
+      let inline adapt  (F f)         = f
+      let inline invoke f fp ps m d   = f fp ps m d
 
-      let inline isGood ft      = FailureTree.IsGood ft
+      let inline left d               = Dispatcher.Left d
+      let inline right d              = Dispatcher.Right d
+      let inline dispatch d v         = Dispatcher.Value d v
 
-      let inline left d         = Dispatcher.Left d
-      let inline right d        = Dispatcher.Right d
-      let inline dispatch d v   = Dispatcher.Value d v
+      let inline zero ()              = LanguagePrimitives.GenericZero<_>
 
+      let inline join l r             =  (^T : (static member Join : ^T * ^T -> ^T) (l, r))
 
-      let inline zero ()        = LanguagePrimitives.GenericZero<_>
+      let inline flatten (l : ^T)     = (^T : (static member Flatten : ^T  -> ^U) l)
 
-      let inline join l r = 
-        (^T : (static member Join : ^T * ^T -> ^T) (l, r))
+      let inline concat ps tps        =
+        if List.isEmpty ps then tps
+        else Seq.append ps tps
 
-      let inline flatten (l : ^T) = 
-        (^T : (static member Flatten : ^T  -> ^U) l)
     open Details
 
     let value v : Formlet<_> =
-      F <| fun fp m d ->
+      F <| fun fp ps m d ->
         v, zero (), zero ()
 
     let bind t uf : Formlet<_> =
       let t = adapt t
-      F <| fun fp m d ->
-
+      F <| fun fp ps m d ->
         let tm, um =
           match m with
           | Model.Fork (l, r) -> l, r
           | _                 -> zero (), zero ()
 
-        let tv, tvt, tft  = invoke t fp tm (left d)
+        let tv, tvt, tft  = invoke t fp ps tm (left d)
         let u             = uf tv
         let u             = adapt u
-        let uv, uvt, uft  = invoke u fp um (right d)
+        let uv, uvt, uft  = invoke u fp ps um (right d)
 
         uv, join tvt uvt, join tft uft
 
     let apply t u : Formlet<_> =
       let t = adapt t
       let u = adapt u
-      F <| fun fp m d ->
-
+      F <| fun fp ps m d ->
         let tm, um =
           match m with
           | Model.Fork (l, r) -> l, r
           | _                 -> zero (), zero ()
 
-        let tv, tvt, tft  = invoke t fp tm (left d)
-        let uv, uvt, uft  = invoke u fp um (right d)
+        let tv, tvt, tft  = invoke t fp ps tm (left d)
+        let uv, uvt, uft  = invoke u fp ps um (right d)
 
         tv uv, join tvt uvt, join tft uft
 
+    let map t f : Formlet<_> =
+      let t = adapt t
+      F <| fun fp ps m d ->
+        let tv, tvt, tft  = invoke t fp ps m d
+        f tv, tvt, tft
+
+    let withAttribute p t : Formlet<_> =
+      let t = adapt t
+      F <| fun fp ps m d ->
+        invoke t fp (p::ps) m d
+
     let textInput initial : Formlet<string> =
-      F <| fun fp m d ->
+      F <| fun fp ps m d ->
         let v = 
           match m with 
           | Model.Value v -> v 
           | _             -> initial
 
-        let vt = ViewTree.Element (input [|Value v; OnChange <| fun v -> dispatch d v.Value|])
+        let vt = ViewTree.Element (input (concat ps [|Value v; OnChange <| fun v -> dispatch d v.Value|]))
 
         v, vt, zero ()
 
@@ -165,216 +169,51 @@ module Formlets2 =
 
     let view t m d : ReactElement =
       let t = adapt t
-      let _, tvt, _ = invoke t [] m (D d)
+      let _, tvt, _ = invoke t [] [] m (D d)
 
       let es = flatten tvt
-      div [] es
+      let re = div [] es
+      re
 
     let rec update mu m : Model =
       match mu, m with
       | ModelUpdate.Value v , _                 -> Model.Value v
-      | ModelUpdate.Left  u , Model.Fork (l, r) -> Model.Fork(update u l, r)
-      | ModelUpdate.Right u , Model.Fork (l, r) -> Model.Fork(l, update u r)
+      | ModelUpdate.Left  u , Model.Fork (l, r) -> Model.Fork (update u l, r)
+      | ModelUpdate.Right u , Model.Fork (l, r) -> Model.Fork (l, update u r)
       // mu is either left or right, create a new fork and update it
       | _                   , _                 -> update mu (Model.Fork (zero (), zero ()))
 
-  type Formlet<'T> with
-    static member (>>=) (t, uf) = Formlet.bind t uf
-    static member (<*>) (f, t)  = Formlet.apply f t
-
-module Formlets =
-  open Fable.Import.React
-
-  [<RequireQualifiedAccess>]
-  type FailurePathElement =
-    | Named   of string
-
-  type FailurePath = FailurePathElement list
-
-  [<RequireQualifiedAccess>]
-  type FailureTree =
-    | Empty
-    | Leaf        of FailurePath*string
-    | Suppress    of FailureTree
-    | Fork        of FailureTree*FailureTree
-
-    member x.IsGood =
-      match x with
-      | Empty
-      | Suppress _  -> true
-      | Leaf _
-      | Fork (_, _) -> false
-
-    static member Zero        : FailureTree = Empty
-    static member Join (l, r) : FailureTree =
-      match l, r with
-      | Empty       , _           -> r
-      | _           , Empty       -> l
-      | Suppress l  , Suppress r  -> Suppress (Fork (l, r))
-      | _           , _           -> Fork (l, r)
-    static member Flatten ft  : (bool*FailurePath*string) list =
-      let rec loop suppress all ft =
-        match ft with
-        | FailureTree.Empty       -> all
-        | FailureTree.Leaf (p, m) -> (suppress, p, m)::all
-        | FailureTree.Suppress ft -> loop true all ft
-        | FailureTree.Fork (l, r) -> loop suppress (loop suppress all l) r
-      let all = loop false [] ft
-      List.rev all
-
-  [<RequireQualifiedAccess>]
-  type StateTree =
-    | Empty
-    | Value   of string ref
-    | Fork    of StateTree*StateTree
-
-    static member Zero        : StateTree = Empty
-    static member Join (l, r) : StateTree = Fork (l, r)
-
-  type ViewTree =
-    | Empty
-    | Element   of ReactElement
-    | Fork      of ViewTree*ViewTree
-
-    static member Zero        : ViewTree = Empty
-    static member Join (l, r) : ViewTree = 
-      match l, r with
-      | Empty       , _           -> r
-      | _           , Empty       -> l
-      | _           , _           -> Fork (l, r)
-    static member Flatten vt  : ReactElement list =
-      let rec loop all vt =
-        match vt with
-        | ViewTree.Empty        -> all
-        | ViewTree.Element e    -> e::all
-        | ViewTree.Fork (l, r)  -> loop (loop all l) r
-      let all = loop [] vt
-      List.rev all
-
-
-  type FormletResult<'T> = FR of 'T*ViewTree*StateTree*FailureTree
-
-  type Formlet<'T> = F of (FailurePath -> StateTree -> FormletResult<'T>)
-
-  module Formlet =
-    module Details =
-      //let inline adapt  f       = OptimizedClosures.FSharpFunc<_, _, _>.Adapt f
-      //let inline invoke f fp st = (f : OptimizedClosures.FSharpFunc<_, _, _>).Invoke (fp, st)
-      let inline adapt  f       = f
-      let inline invoke f fp st = f fp st
-
-      let inline isGood (ft : FailureTree) = ft.IsGood
-
-      let inline zero ()        = LanguagePrimitives.GenericZero<_>
-
-      let inline join l r = 
-        (^T : (static member Join : ^T * ^T -> ^T) (l, r))
-
-      let inline flatten (l : ^T) = 
-        (^T : (static member Flatten : ^T  -> ^U) l)
-
-    open Details
-
-    let inline value v : Formlet<_> =
-      F <| fun fp st ->
-        FR (v, zero (), zero (), zero ())
-
-    let inline bind (F t) (uf : 'T -> Formlet<'U>) : Formlet<'U> =
+    let collect t m =
       let t = adapt t
-      F <| fun fp st ->
-        let tst, ust = 
-          match st with
-          | StateTree.Fork (tst, ust) -> tst, ust
-          | _                         -> zero (), zero ()
+      let tv, _, tft = invoke t [] [] m (D (fun _ -> ()))
 
-        let (FR (tv, tvt, tst, tft)) = invoke t fp tst
-        let (F u) = uf tv
-        let u = adapt u
-        let (FR (uv, uvt, ust, uft)) = invoke u fp ust
-
-        FR (uv, join tvt uvt, join tst ust, join tft uft)
-
-    let inline apply (F f) (F t) : Formlet<'U> =
-      let f = adapt f
-      let t = adapt t
-      F <| fun fp st ->
-        let fst, tst = 
-          match st with
-          | StateTree.Fork (fst, tst) -> fst, tst
-          | _                         -> zero (), zero ()
-
-        let (FR (fv, fvt, fst, fft)) = invoke f fp fst
-        let (FR (tv, tvt, tst, tft)) = invoke t fp tst
-
-        FR (fv tv, join fvt tvt, join fst tst, join fft tft)
-
-    let textInput initial : Formlet<string> =
-      F <| fun fp st ->
-        let st, v = 
-          match st with 
-          | StateTree.Value v -> st, !v 
-          | _                 -> StateTree.Value (ref initial), initial
-
-        let vt = ViewTree.Element (input [|Value v|])
-
-        FR (v, vt, st, zero ())
-
-    let eval st (F t) =
-      let t = adapt t
-      let (FR (v, vt, st, ft)) = invoke t [] st
-      let es = flatten vt
-      let fs = flatten ft
-      let e = div [] es
-      (v, e, st, fs)
+      let fs = flatten tft
+      tv, fs
 
   type Formlet<'T> with
-    static member (>>=) (t, uf) = Formlet.bind t uf
+    static member (>>=) (t, uf) = Formlet.bind  t uf
     static member (<*>) (f, t)  = Formlet.apply f t
+    static member (|>>) (t, f)  = Formlet.map   t f
 
-module Test =
-  open Formlets
-  open Formlet
+open Formlets
 
-  let test = 
-    value (fun f s -> f, s)
-    <*> textInput "Hello"
-    <*> textInput "There"
+let form = 
+  Formlet.value (fun f s -> f, s)
+  <*> (Formlet.textInput "Hello" |> Formlet.withAttribute (Class "test"))
+  <*> Formlet.textInput "There"
+
+let init () = Model.Empty
+
+let update msg model = 
+  let before = model
+  printfn "Update - msg   : %A" msg
+  printfn "Update - before: %A" before
+  let after  = Formlet.update msg model
+  printfn "Update - after : %A" after
+  after
+
+let view model dispatch = Formlet.view form model dispatch
   
-  let render () =
-    let (s, re, st, fs) = eval StateTree.Empty test
-    printfn "Result   : %A" s
-    printfn "StateTree: %A" st
-    printfn "Failures : %A" fs
-    re
-
-// MODEL
-
-type Model = int
-
-type Msg =
-| Increment
-| Decrement
-
-let init() : Model = 0
-
-// UPDATE
-
-let update (msg:Msg) (model:Model) =
-    match msg with
-    | Increment -> model + 1
-    | Decrement -> model - 1
-
-// VIEW (rendered with React)
-
-let view (model:Model) dispatch =
-  let t = Test.render ()
-
-  div []
-      [ button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
-        div [] [ str (string model) ]
-        button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] ]
-
-
 // App
 Program.mkSimple init update view
 |> Program.withReact "elmish-app"
